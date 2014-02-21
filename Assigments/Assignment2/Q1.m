fc22889ad35416ec204aa93c1a4ce1ac41d83662
v3c70r <mcvector.gu@gmail.com>
2014-02-20 21:14:33 -0500

    Assignment 2 almost finished

--- Assigments/Assignment2/Q1.m
+++ Assigments/Assignment2/Q1.m
@@ -6,6 +6,7 @@ function Q1()
 
     clf();
 
+    figure(1);
     hold on;
     for lambda=0:1:100
         W = L2Regression(X(1:80,:), Y(1:80), lambda);
@@ -14,8 +15,52 @@ function Q1()
         rootMeanSqrErr = sqrt( (sum((newY-Y(1:80)).^2)/length(Y(1:80))));
         rootMeanSqrErrTest = sqrt( (sum((testY-Y(81:99)).^2)/length(Y(81:99))));
         scatter(lambda, rootMeanSqrErr );
-        scatter(lambda, rootMeanSqrErrTest,'ok');
+        scatter(lambda, rootMeanSqrErrTest,[],[2 0 0]);     %Red
     end
     hold off;
+
+    print('L1Err.eps', '-depsc');
+
+    
+
+    figure(2);
+    hold on;
+    for lambda=0:1:100
+        W = L2Regression(X(1:80,:), Y(1:80), lambda);
+        scatter(lambda, W(1),[], [1 0 0]);
+        scatter(lambda, W(2),[], [0 1 0]);
+        scatter(lambda, W(3),[], [0 0 1]);
+        scatter(lambda, W(4),[], [1 0 1]);
+    end
+    hold off;
+    print('L1weight.eps', '-depsc');
+
+
+    %L2 regression
+    figure(3);
+    hold on;
+    for lambda=0:1:100
+        W = L2Regression(X, Y, 1);
+        newY = extendInput(X(1:80, :)) * W;
+        testY = extendInput(X(81:99, :))*W;
+        rootMeanSqrErr = sqrt( (sum((newY-Y(1:80)).^2)/length(Y(1:80))));
+        rootMeanSqrErrTest = sqrt( (sum((testY-Y(81:99)).^2)/length(Y(81:99))));
+        scatter(lambda, rootMeanSqrErr );
+        scatter(lambda, rootMeanSqrErrTest,[],[2 0 0]);     %Red
+    end
+    hold off;
+    print('L2err.eps', '-depsc');
+
+    figure(4);
+    hold on;
+    for lambda=0:1:100
+        W = L2Regression(X(1:80,:), Y(1:80), lambda);
+        scatter(lambda, W(1),[], [1 0 0]);
+        scatter(lambda, W(2),[], [0 1 0]);
+        scatter(lambda, W(3),[], [0 0 1]);
+    end;
+    hold off;
+    print('L2weight.eps', '-depsc');
+
 
 end
